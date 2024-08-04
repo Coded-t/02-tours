79520b166b2c396c0c4e54c8645811bed983e7dc
Coded-t <belloharuna211@gmail.com>
2024-08-04 22:39:35 +0100

    A web store app

---
+++ 03-advanced-react/src/App.jsx
@@ -0,0 +1,13 @@
+import ReducerBasics from "./tutorial/10-useReducer/final/01-useReducer";
+
+function App() {
+  return (
+    <div className='container'>
+      <h2>Advanced React</h2>
+
+    <ReducerBasics/>
+    </div>
+  );
+}
+
+export default App;
